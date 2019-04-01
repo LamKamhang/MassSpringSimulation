439ae467bc6025dbe02d05725fbefd89e9ff1325
LamKamhang <Cool_Lam@outlook.com>
2019-04-01 23:55:10 +0800

    add chainSpring1D system, but lack some function

--- src/simulation.cpp
+++ src/simulation.cpp
@@ -10,39 +10,53 @@
 using namespace std;
 using namespace mymath;
 
-void render(unsigned left, unsigned right)
-{
-	for (unsigned i = 0; i < left; ++i)
-	{
-		cout << " ";
-	}
-	for (unsigned i = left; i <= right; ++i)
-	{
-		cout << "*";
-	}
-	cout  << endl;
-}
+// void render(unsigned left, unsigned right)
+// {
+// 	for (unsigned i = 0; i < left; ++i)
+// 	{
+// 		cout << " ";
+// 	}
+// 	for (unsigned i = left; i <= right; ++i)
+// 	{
+// 		cout << "*";
+// 	}
+// 	cout  << endl;
+// }
 
 int main(int argc, char const *argv[])
 {
-	shared_ptr<Spring> spring(new Spring(50, 20));
-	shared_ptr<Particle> left(new Particle(300, false, {10, 0, 0}));
-	shared_ptr<Particle> right(new Particle(200, false, {20, 0, 0}));
+	// shared_ptr<Spring> spring(new Spring(50, 20));
+	// shared_ptr<Particle> left(new Particle(300, false, {10, 0, 0}));
+	// shared_ptr<Particle> right(new Particle(200, false, {20, 0, 0}));
 	
-	spring->bindLeft(left);
-	spring->bindRight(right);
+	// spring->bindLeft(left);
+	// spring->bindRight(right);
 
-	NaiveSystem ns(spring);
+	// NaiveSystem ns(spring);
 
-	while (1)
-	{
-		ns.update(0.1);
-		unsigned x0 = left->getPos().getItem(0)*5;
-		unsigned x1 = right->getPos().getItem(0)*5;
-		// cout << x0 << " " << x1 << endl;
-		render(x0, x1);
-		usleep(100000);
-	}
+	// while (1)
+	// {
+	// 	ns.update(0.1);
+	// 	unsigned x0 = left->getPos().getItem(0)*5;
+	// 	unsigned x1 = right->getPos().getItem(0)*5;
+	// 	// cout << x0 << " " << x1 << endl;
+	// 	render(x0, x1);
+	// 	usleep(100000);
+	// }
+
+	shared_ptr<Particle> m1(new Particle(100, true));
+	shared_ptr<Particle> m2(new Particle(200, true));
+	shared_ptr<Particle> m3(new Particle(300, true));
+	shared_ptr<Particle> m4(new Particle(400, true));
+
+	shared_ptr<Spring> s1(new Spring(4, 100));
+	shared_ptr<Spring> s2(new Spring(8, 100));
+	shared_ptr<Spring> s3(new Spring(16, 100));
+	
+	vector<shared_ptr<Particle>* >mass{&m1, &m2, &m3, &m4};
+	vector<shared_ptr<Spring>* > springs{&s1, &s2, &s3};
+
+	ChainSystem1D(mass, springs);
 
 	return 0;
 }
--- src/simulation.hpp
+++ src/simulation.hpp
@@ -7,6 +7,9 @@
 #include "spring.hpp"
 #include "vector.hpp"
 
+#include <eigen3/Eigen/Sparse>
+#include <eigen3/Eigen/Cholesky>
+
 class NaiveSystem
 {
 public:
@@ -72,4 +75,96 @@ void NaiveSystem::_ImplicitEuler(double h)
 	// clean force
 	left->setForce({0, 0, 0});
 	right->setForce({0, 0, 0});
+}
+
+class ChainSystem1D
+{
+public:
+	ChainSystem1D(const std::vector<std::shared_ptr<Particle>* > &mass,
+				  const std::vector<std::shared_ptr<Spring>* > &springs,
+				  double h = 0.1);
+	void update();
+
+private:
+	void _ImplicitEuler();
+
+private:
+	std::vector<std::shared_ptr<Particle>* > _mass;
+	std::vector<std::shared_ptr<Spring>* > _springs;
+	double timeStep;
+	Eigen::MatrixXd A;
+	Eigen::MatrixXd K;
+};
+
+ChainSystem1D::ChainSystem1D(const std::vector<std::shared_ptr<Particle>* > &mass,
+							 const std::vector<std::shared_ptr<Spring>* > &springs,
+							 double h)
+	: _mass(mass)
+	, _springs(springs)
+	, timeStep(h)
+	, A(_mass.size(), _mass.size())
+	, K(_mass.size(), _mass.size())
+{
+	auto size = _mass.size();
+
+	// frst line
+	K(0, 0) = (*springs[0])->getK();
+	K(0, 1) = -(*springs[0])->getK();
+	for (unsigned i = 2; i < size; ++i)
+	{
+		K(0, i) = 0;
+	}
+
+	// 2 to n-1
+	for (unsigned i = 1; i < size-1; ++i)
+	{
+		for (unsigned j = 0; j < i-1; ++j)
+		{
+			K(i, j) = 0;
+		}
+		K(i, i-1) = -(*springs[i-1])->getK();
+		K(i, i) = (*springs[i-1])->getK()+(*springs[i])->getK();
+		K(i, i+1) = -(*springs[i])->getK();
+		for (unsigned j = i+2; j < size; ++j)
+		{
+			K(i, j) = 0;
+		}
+	}
+
+	// last line
+	for (unsigned i = 0; i < size - 2; ++i)
+	{
+		K(size-1, i) = 0;
+	}
+
+	K(size-1, size-2) = -(*springs[size-2])->getK();
+	K(size-1, size-1) = (*springs[size-2])->getK();
+
+	std::cout << K << std::endl;
+
+	for (unsigned i = 0; i < size; ++i)
+	{
+		for (unsigned j = 0; j < i; ++j)
+		{
+			A(i, j) = 0;
+		}
+		A(i, i) = (*_mass[i])->getMass();
+		for (unsigned j = i+1; j < size; ++j)
+		{
+			A(i, j) = 0;
+		}
+	}
+
+	std::cout << "A" << std::endl;
+	std::cout << A << std::endl;
+
+	A += h*h*K;
+
+	std::cout << "A" << std::endl;
+	std::cout << A << std::endl;
+}
+
+void ChainSystem1D::_ImplicitEuler()
+{
+
 }
