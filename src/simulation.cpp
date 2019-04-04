a510d75067dc516f53e5c18456a2806bd692bc8b
LamKamhang <Cool_Lam@outlook.com>
2019-04-05 00:33:02 +0800

    add testing simulation

--- src/simulation.cpp
+++ src/simulation.cpp
@@ -55,25 +55,40 @@ int main(int argc, char const *argv[])
 	// shared_ptr<Particle> m3(new Particle(4, false, {200, 0, 0}));
 	// shared_ptr<Particle> m4(new Particle(8, false, {250, 0, 0}, {-50, 0, 0}));
 
-	shared_ptr<Spring> spring(new Spring(50, 20));
-	shared_ptr<Particle> left(new Particle(300, true, {10, 0, 0}));
-	shared_ptr<Particle> right(new Particle(200, false, {20, 0, 0}));
+	// shared_ptr<Spring> spring(new Spring(50, 20));
+	// shared_ptr<Particle> left(new Particle(300, true, {10, 0, 0}));
+	// shared_ptr<Particle> right(new Particle(200, false, {20, 0, 0}));
 	
-	spring->bindLeft(left);
-	spring->bindRight(right);
+	// spring->bindLeft(left);
+	// spring->bindRight(right);
 	
-	vector<shared_ptr<Particle>* >mass{&left, &right};
-	vector<shared_ptr<Spring>* > springs{&spring};
+	// vector<shared_ptr<Particle>* >mass{&left, &right};
+	// vector<shared_ptr<Spring>* > springs{&spring};
+
+	// ChainSystem1D test(mass, springs);
+
+		shared_ptr<Particle> m1(new Particle(10, true));
+	shared_ptr<Particle> m2(new Particle(10, false, {1, 0, 0}));
+	// shared_ptr<Particle> m3(new Particle(10, false, {3, 0, 0}));
+	// shared_ptr<Particle> m4(new Particle(15, false, {6, 0, 0}));
+
+	shared_ptr<Spring> s1(new Spring(5, 3));
+	// shared_ptr<Spring> s2(new Spring(4, 2, m2, m3));
+	// shared_ptr<Spring> s3(new Spring(3, 4, m3, m4));
+	s1->bindLeft(m1);
+	s1->bindRight(m2);
+	vector<shared_ptr<Particle>* >mass{&m1, &m2/*, &m3, &m4*/};
+	vector<shared_ptr<Spring>* > springs{&s1/*, &s2, &s3*/};
 
 	ChainSystem1D test(mass, springs);
 	
 	while (1)
 	{
 		test.update();
-		unsigned x0 = left->getPos().getItem(0)*5;
-		unsigned x1 = right->getPos().getItem(0)*5;
-		// cout << x0 << " " << x1 << endl;
-		render(x0, x1);
+		unsigned x0 = m1->getPos().getItem(0)*5;
+		unsigned x1 = m2->getPos().getItem(0)*5;
+		cout << m1->getPos().getItem(0) << " " << m2->getPos().getItem(0) << endl;
+		// render(x0, x1);
 		usleep(100000);
 	}
 
