e8ee0cd3dc931ced6ebf2fefc452637324141eba
LamKamhang <Cool_Lam@outlook.com>
2019-05-15 01:19:42 +0800

    modify simulation, add gravity and fix bugs

--- new_version/src/simulation.hxx
+++ new_version/src/simulation.hxx
@@ -14,7 +14,8 @@
 #include "spring.hxx"
 
 #define EPSILON 1e-4
-#define NEWTON_STEPS 5
+#define NEWTON_STEPS 10
+#define MINIMUM_LENGTH 0.1
 
 class NetSystem
 {
@@ -28,6 +29,11 @@ public:
 
 	inline void setH(double h);
 	inline double getH();
+
+	inline const std::vector<std::shared_ptr<Particle> >& get_particle_vec() const;
+	inline const std::vector<std::shared_ptr<Spring > >& get_spring_vec() const;
+	inline const Eigen::VectorXd& get_x() const;
+	inline const Eigen::VectorXd& get_v() const;
 
 	inline unsigned add_particle(Eigen::Vector3d x, Eigen::Vector3d v = {0, 0, 0}, bool fixed = false, double mass = 1);
 	inline unsigned add_particle(Particle *ptr);
@@ -83,6 +89,27 @@ void NetSystem::setAssembleable(bool flag)
 	if (!_assembleable)
 		_setup();
 }
+
+const std::vector<std::shared_ptr<Particle> >& NetSystem::get_particle_vec() const
+{
+	return _particle_vec;
+}
+
+const std::vector<std::shared_ptr<Spring > >& NetSystem::get_spring_vec() const
+{
+	return _spring_vec;
+}
+
+const Eigen::VectorXd& NetSystem::get_x() const
+{
+	return _xt;
+}
+
+const Eigen::VectorXd& NetSystem::get_v() const
+{
+	return _vt;
+}
+
 
 bool NetSystem::getAssembleable()
 {
@@ -143,9 +170,10 @@ void NetSystem::_setup()
 	_vt = Eigen::VectorXd(_particle_vec.size() * 3);
 	_mass = Eigen::MatrixXd(_particle_vec.size() * 3, _particle_vec.size() * 3);
 
-	int i = 0;
+	int i;
 	for (auto item : _particle_vec)
 	{
+		i = 3*item->getID();
 		Eigen::Vector3d X = item->getX();
 		Eigen::Vector3d V = item->getV();
 		for (int j = 0; j < 3; ++j)
@@ -154,18 +182,183 @@ void NetSystem::_setup()
 			_xt(i+j) = X(j);
 			_vt(i+j) = V(j);
 		}
-		i = i + 3;
 	}
+#ifdef DEBUG
+	std::cout << "mass" << std::endl;
+	std::cout << _mass << std::endl << std::endl;
+#endif
 }
 
 void NetSystem::_update()
 {
-	int i = 0;
-	for (auto item : _particle_vec)
+	// int i;
+	// for (auto item : _particle_vec)
+	// {
+	// 	i = 3*item->getID();
+	// 	Eigen::Vector3d X = item->getX();
+	// 	Eigen::Vector3d V = item->getV();
+	// 	if (item->getFixStatus())
+	// 	{
+	// 		for (int j = 0; j < 3; ++j)
+	// 		{
+	// 			_xt(i+j) = X(j);
+	// 			_vt(i+j) = V(j);
+	// 		}
+	// 	}
+	// 	else
+	// 	{
+	// 		item->setX({_xt(i), _xt(i+1), _xt(i+2)});
+	// 		item->setV({_vt(i), _vt(i+1), _vt(i+2)});
+	// 	}
+	// }
+
+	for (auto spring : _spring_vec)
 	{
-		item->setX({_xt(i), _xt(i+1), _xt(i+1)});
-		item->setV({_vt(i), _vt(i+1), _vt(i+1)});
-		i+=3;
+		auto lid = spring->getLID();
+		auto rid = spring->getRID();
+
+		auto lp = _particle_vec[lid];
+		auto rp = _particle_vec[rid];
+
+		lid *= 3;
+		rid *= 3;
+		Eigen::Vector3d l_X = lp->getX();
+		Eigen::Vector3d l_V = lp->getV();
+		Eigen::Vector3d r_X = rp->getX();
+		Eigen::Vector3d r_V = rp->getV();
+
+		Eigen::Vector3d l_nX(_xt(lid), _xt(lid+1), _xt(lid+2));
+		Eigen::Vector3d r_nX(_xt(rid), _xt(rid+1), _xt(rid+2));
+
+		Eigen::Vector3d l_nV(_vt(lid), _vt(lid+1), _vt(lid+2));
+		Eigen::Vector3d r_nV(_vt(rid), _vt(rid+1), _vt(rid+2));
+
+		if (lp->getFixStatus())
+		{
+			for (int j = 0; j < 3; ++j)
+			{
+				_xt(lid+j) = l_X(j);
+				_vt(lid+j) = l_V(j);
+			}
+		}
+		else
+		{
+			if ((l_nX - r_nX).norm() <= MINIMUM_LENGTH)
+			{
+				for (int j = 0; j < 3; ++j)
+				{
+					_xt(lid+j) = l_X(j);
+					_vt(lid+j) = _vt(rid+j);
+				}
+				lp->setV({_vt(lid), _vt(lid+1), _vt(lid+2)});
+			}
+			else
+			{
+				lp->setX({_xt(lid), _xt(lid+1), _xt(lid+2)});
+				lp->setV({_vt(lid), _vt(lid+1), _vt(lid+2)});
+			}
+		}
+		
+		if (rp->getFixStatus())
+		{
+			for (int j = 0; j < 3; ++j)
+			{
+				_xt(rid+j) = r_X(j);
+				_vt(rid+j) = r_V(j);
+			}
+		}
+		else
+		{
+			if ((l_nX - r_nX).norm() <= MINIMUM_LENGTH)
+			{
+				for (int j = 0; j < 3; ++j)
+				{
+					_xt(rid+j) = r_X(j);
+					_vt(rid+j) = _vt(lid+j);
+				}
+				rp->setV({_vt(rid), _vt(rid+1), _vt(rid+2)});
+			}
+			else
+			{
+				rp->setX({_xt(rid), _xt(rid+1), _xt(rid+2)});
+				rp->setV({_vt(rid), _vt(rid+1), _vt(rid+2)});
+			}
+		}
+
+		// if ((l_nX - r_nX).norm() <= MINIMUM_LENGTH)
+		// {
+		// 	i = lp->getID()*3;
+		// 	if (lp->getFixStatus())
+		// 	{
+		// 		for (int j = 0; j < 3; ++j)
+		// 		{
+		// 			_xt(i+j) = l_X(j);
+		// 			_vt(i+j) = l_V(j);
+		// 		}
+		// 	}
+		// 	else
+		// 	{
+		// 		for (int j = 0; j < 3; ++j)
+		// 		{
+		// 			_xt(i+j) = l_X(j);
+		// 			_vt(i+j) = -_vt(i+j);
+		// 		_vt(i+j) = 0;
+		// 		}
+		// 		lp->setV({_vt(i), _vt(i+1), _vt(i+2)});
+		// 	}
+			
+		// 	i = rp->getID()*3;
+		// 	if (rp->getFixStatus())
+		// 	{
+		// 		for (int j = 0; j < 3; ++j)
+		// 		{
+		// 			_xt(i+j) = r_X(j);
+		// 			_vt(i+j) = r_V(j);
+		// 		}
+		// 	}
+		// 	else
+		// 	{
+		// 		for (int j = 0; j < 3; ++j)
+		// 		{
+		// 			_xt(i+j) = r_X(j);
+		// 			_vt(i+j) = -_vt(i+j);
+		// 			_vt(i+j) = 0;
+		// 		}
+		// 		rp->setV({_vt(i), _vt(i+1), _vt(i+2)});
+		// 	}
+		// }
+		// else
+		// {
+		// 	i = lp->getID()*3;
+		// 	if (lp->getFixStatus())
+		// 	{
+		// 		for (int j = 0; j < 3; ++j)
+		// 		{
+		// 			_xt(i+j) = l_X(j);
+		// 			_vt(i+j) = l_V(j);
+		// 		}
+		// 	}
+		// 	else
+		// 	{
+		// 		lp->setX({_xt(i), _xt(i+1), _xt(i+2)});
+		// 		lp->setV({_vt(i), _vt(i+1), _vt(i+2)});
+		// 	}
+			
+		// 	i = rp->getID()*3;
+		// 	if (rp->getFixStatus())
+		// 	{
+		// 		for (int j = 0; j < 3; ++j)
+		// 		{
+		// 			_xt(i+j) = r_X(j);
+		// 			_vt(i+j) = r_V(j);
+		// 		}
+		// 	}
+		// 	else
+		// 	{
+		// 		rp->setX({_xt(i), _xt(i+1), _xt(i+2)});
+		// 		rp->setV({_vt(i), _vt(i+1), _vt(i+2)});
+		// 	}
+		// }
 	}
 }
 
@@ -177,8 +370,14 @@ Eigen::VectorXd NetSystem::_f(const Eigen::VectorXd &x)
 	Eigen::VectorXd grad = Eigen::VectorXd::Zero(_xt.size());
 	for (auto spring : _spring_vec)
 		spring->accumulate_grad(grad, x);
+#ifdef DEBUG
+	std::cout << "grad" << std::endl;
+	std::cout << grad << std::endl << std::endl;
+#endif
+	// for (auto particle : _particle_vec)
+	// 	particle->accumulate_grad(grad, x);
 	
-	return _mass * (x - _xt - _h*_vt) + _h*_h*grad;
+	return _mass * ((x - _xt)/(_h*_h) - _vt/_h) + grad;
 }
 
 Eigen::MatrixXd NetSystem::_df(const Eigen::VectorXd &x)
@@ -189,12 +388,19 @@ Eigen::MatrixXd NetSystem::_df(const Eigen::VectorXd &x)
 	Eigen::MatrixXd hessian = Eigen::MatrixXd::Zero(_xt.size(), _xt.size());
 	for (auto spring: _spring_vec)
 		spring->accumulate_hessian(hessian, x);
-	
-	return _mass + _h*_h*hessian;
+#ifdef DEBUG
+	std::cout << "hessian" << std::endl;
+	std::cout << hessian << std::endl << std::endl;
+#endif
+	return _mass / (_h*_h) + hessian;
 }
 
 Eigen::VectorXd NetSystem::_newton_iterate()
 {
+#ifdef DEBUG
+	std::cout << _xt << std::endl << std::endl;
+	std::cout << _f(_xt) << std::endl << std::endl;
+#endif
 	Eigen::VectorXd xtt = _xt;
 	Eigen::VectorXd b, deltaX;
 	Eigen::MatrixXd A;
@@ -202,18 +408,27 @@ Eigen::VectorXd NetSystem::_newton_iterate()
 	for (int i = 0; i < NEWTON_STEPS; ++i)
 	{
 		b = -_f(xtt);
+		double norm = b.norm();
+		if (norm < EPSILON && norm > -EPSILON)
+		{
+#ifdef DEBUG
+			std::cout << "finish newton iterate, " << i << " steps" << std::endl;
+#endif
+			break;
+		}	
 		A = _df(xtt);
 		deltaX = A.ldlt().solve(b);
+#ifdef DEBUG
+	std::cout << b << std::endl;
+	std::cout << A << std::endl;
+	std::cout << deltaX << std::endl;
+#endif
 		xtt += deltaX;
 	}
 #ifdef DEBUG
+	std::cout << xtt << std::endl << std::endl;
 	std::cout << "newton iterate" << std::endl;
 	std::cout << _f(xtt) << std::endl << std::endl;
-	Eigen::VectorXd grad = Eigen::VectorXd::Zero(_xt.size());
-	for (auto spring : _spring_vec)
-		spring->accumulate_grad(grad, xtt);
-	std::cout << "force" << std::endl;
-	std::cout << grad << std::endl << std::endl;
 #endif
 
 	return xtt;
@@ -229,7 +444,19 @@ void NetSystem::simulate()
 	_xt = xtt;
 	_vt = deltaX / _h;
 
+
 	_update();
+	std::cout << "force: " << std::endl;
+	Eigen::VectorXd grad = Eigen::VectorXd::Zero(_xt.size());
+	for (auto spring : _spring_vec)
+		spring->accumulate_grad(grad, _xt);
+	std::cout << -grad << std::endl << std::endl;
+
+	std::cout << "pos:" << std::endl;
+	std::cout << _xt << std::endl << std::endl;
+
+	std::cout << "vec:" << std::endl;
+	std::cout << _vt << std::endl << std::endl;
 }
 
 
