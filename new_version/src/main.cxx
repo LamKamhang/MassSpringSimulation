19e7e5c759a32b764fb3c6b1ae1061339cabbde1
LamKamhang <Cool_Lam@outlook.com>
2019-05-15 01:19:58 +0800

    add main file

--- new_version/src/main.cxx
+++ new_version/src/main.cxx
@@ -1,9 +1,11 @@
 #include "spring.hxx"
 #include "particle.hxx"
 #include "simulation.hxx"
+#include "renderer.hxx"
 
 #include <unistd.h>
 #include <iostream>
+#include <memory>
 #include <Eigen/Core>
 
 using namespace std;
@@ -11,27 +13,38 @@ using namespace Eigen;
 
 int main(int argc, char const *argv[])
 {
-	NetSystem spring_system;
-	spring_system.add_particle({0,0,0});
-	spring_system.add_particle({1,0,0});
-	spring_system.add_particle({0,1,0});
-	spring_system.add_particle({0,0,1});
-	spring_system.add_particle({1,1,0});
+	shared_ptr<NetSystem> spring_system(new NetSystem);
+	spring_system->add_particle({0,0,0}, {0,0,0}, true);
+	spring_system->add_particle({1,0,0});
+	spring_system->add_particle({2,0,0});
+	// spring_system->add_particle({3,0,0});
+	// spring_system->add_particle({1,1,1});
+	// spring_system->add_particle({4,1,1});
+	// spring_system->add_particle({5,1,3});
+	// spring_system->add_particle({6,7,8});
+	// spring_system->add_particle({7,8,9});
 
-	spring_system.add_spring(0, 1, 1, 0.5);
-	spring_system.add_spring(1, 2, 1, 0.5);
-	spring_system.add_spring(2, 3, 1, 0.5);
-	spring_system.add_spring(3, 4, 1, 0.5);
-	
-	spring_system.assemble_complete();
+	spring_system->add_spring(0, 1, 1, 0.5);
+	spring_system->add_spring(1, 2, 1, 0.5);
+	// spring_system->add_spring(2, 3, 1, 0.5);
+	// spring_system->add_spring(1, 4, 3, 1);
+	// spring_system->add_spring(4, 5, 3, 1);
+	// spring_system->add_spring(3, 6, 3, 1);
+	// spring_system->add_spring(2, 3, 3, 1);
+		
+	spring_system->assemble_complete();
 
-	while (true)
-	{
-		spring_system.render();
-		spring_system.simulate();
-		cout << "----------------" << endl << endl;
-		// sleep(1);
-	}
+	Renderer renderer;
+	renderer.AddTimerEvent(spring_system);
+
+	renderer.render();
+	// while (true)
+	// {
+	// 	spring_system->render();
+	// 	spring_system->simulate();
+	// 	cout << "----------------" << endl << endl;
+	// 	sleep(1);
+	// }
 	
 	return 0;
 }
