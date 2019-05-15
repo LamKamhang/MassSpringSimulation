9dbc82ecd82d182385ffe7e7eb800a287ef8e2ee
LamKamhang <Cool_Lam@outlook.com>
2019-05-15 23:00:14 +0800

    let renderer class more clear:

--- src/renderer.hxx
+++ src/renderer.hxx
@@ -23,66 +23,8 @@
 #include <vtkTransformPolyDataFilter.h>
 #include <vtkCallbackCommand.h>
 
-class SpringLine
-{
-public:
-	SpringLine(double x1, double y1, double z1, double x2, double y2, double z2);
-	vtkSmartPointer<vtkActor> getActor() const;
-	void update(double x1, double y1, double z1, double x2, double y2, double z2);
-private:
+#include "SystemTimerCallback.hxx"
 
-private:
-	vtkSmartPointer<vtkActor> _actor;
-public:
-	vtkSmartPointer<vtkLineSource> line_src;
-	unsigned lid, rid;
-};
-
-class SystemTimerCallback : public vtkCommand
-{
-public:
-	static SystemTimerCallback *New()
-	{
-		SystemTimerCallback *cb = new SystemTimerCallback;
-		return cb;
-	}
-
-	virtual void Execute(vtkObject * caller, unsigned long vtkNotUsed(eventId),
-						void * vtkNotUsed(callData))
-	{
-		_system->simulate();
-		_system->render();
-		
-		auto xt = _system->get_x();
-		auto spring_vec = _system->get_spring_vec();
-
-		for (auto line: lines_vec)
-		{
-			unsigned lid = line->lid;
-			unsigned rid = line->rid;
-
-			line->line_src->SetPoint1(xt(3*lid), xt(3*lid+1), xt(3*lid+2));
-			line->line_src->SetPoint2(xt(3*rid), xt(3*rid+1), xt(3*rid+2));
-			line->line_src->Update();
-		}
-		vtkRenderWindowInteractor *iren = vtkRenderWindowInteractor::SafeDownCast(caller);
-      	iren->GetRenderWindow()->Render();
-	}
-
-	void setSystem(const std::shared_ptr<NetSystem> &system)
-	{
-		_system = system;
-	}
-
-	void add_line(SpringLine *ptr)
-	{
-		lines_vec.emplace_back(ptr);
-	}
-
-private:
-	std::shared_ptr<NetSystem> _system;
-	std::vector<std::shared_ptr<SpringLine> > lines_vec;
-};
 
 class Renderer
 {
@@ -112,13 +54,13 @@ Renderer::Renderer()
 
 	renderWindow->AddRenderer(renderer);
 	renderWindow->SetWindowName("Mass Springs System");
-	renderWindow->SetSize(600, 800);
+	renderWindow->SetSize(1200, 1600);
 	
 	renderWindowInteractor->SetRenderWindow(renderWindow);
 
 	// Initialize must be called prior to creating timer events.
 	renderWindowInteractor->Initialize();
-	renderWindowInteractor->CreateRepeatingTimer(100);
+	renderWindowInteractor->CreateRepeatingTimer(10);
 }
 
 void Renderer::AddTimerEvent(const std::shared_ptr<NetSystem> &system)
@@ -147,35 +89,4 @@ void Renderer::AddTimerEvent(const std::shared_ptr<NetSystem> &system)
 void Renderer::render()
 {
 	renderWindowInteractor->Start();
-	renderWindowInteractor->Render();
-}
-
-SpringLine::SpringLine(double x1, double y1, double z1, double x2, double y2, double z2)
-{
-	line_src = vtkSmartPointer<vtkLineSource>::New();
-	line_src->SetResolution(20);
-	line_src->SetPoint1(x1, y1, z1);
-	line_src->SetPoint2(x2, y2, z2);
-	line_src->Update();
-
-	vtkSmartPointer<vtkPolyDataMapper> mapper = vtkSmartPointer<vtkPolyDataMapper>::New();
-	mapper->SetInputConnection(line_src->GetOutputPort());
-
-	_actor = vtkSmartPointer<vtkActor>::New();
-	_actor->SetMapper(mapper);
-	_actor->GetProperty()->SetLineWidth(20);
-	srand(time(NULL)*(1+x1*32+x2*16+y1*8+y2*4+z1*2+z2));
-	_actor->GetProperty()->SetColor(((rand()%100) / 100.0), ((rand()%100) / 100.0), ((rand()%100) / 100.0));
-}
-
-void SpringLine::update(double x1, double y1, double z1, double x2, double y2, double z2)
-{
-	line_src->SetPoint1(x1, y1, z1);
-	line_src->SetPoint2(x2, y2, z2);
-	line_src->Update();
-}
-
-vtkSmartPointer<vtkActor> SpringLine::getActor() const
-{
-	return _actor;
 }
