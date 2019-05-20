8bb9a1d7c9169bcc1f7afb99a55b8105fa212571
LamKamhang <Cool_Lam@outlook.com>
2019-05-20 08:48:09 +0800

    another larger case test

--- src/main.cxx
+++ src/main.cxx
@@ -1,21 +1,23 @@
 #include "System/spring.h"
 #include "System/particle.h"
 #include "System/simulation.h"
+#include "System/assemble.hxx"
 
 #include "Render/renderer.h"
 
-#include <unistd.h>
+#include "igl/readOBJ.h"
+
+// #include <unistd.h>
 #include <iostream>
+#include <string>
 #include <memory>
 #include <Eigen/Core>
-#include "System/assemble.hxx"
-#include "igl/readOBJ.h"
 
 using namespace std;
 using namespace Eigen;
 
-#define CLOTH_WIDTH 12
-#define CLOTH_LENGTH 12
+#define CLOTH_WIDTH 15
+#define CLOTH_LENGTH 15
 
 
 int main(int argc, char const *argv[])
@@ -23,11 +25,15 @@ int main(int argc, char const *argv[])
 	shared_ptr<NetSystem> spring_system(new NetSystem);
 	Renderer renderer;
 
-	assemble(spring_system, "resource/mini_cloth.obj");
+	string obj_path;
+	cout << "the obj file path: ";
+	cin >> obj_path;
+
+	assemble(spring_system, obj_path);
 	// assemble(spring_system, CLOTH_WIDTH, CLOTH_LENGTH);
 
 	(spring_system->get_particle_vec())[0]->setFix(true);
-	(spring_system->get_particle_vec())[CLOTH_LENGTH*(CLOTH_LENGTH-1)]->setFix(true);
+	(spring_system->get_particle_vec())[CLOTH_LENGTH*(CLOTH_WIDTH-1)]->setFix(true);
 	spring_system->assemble_complete();
 
 	renderer.AddTimerEvent(spring_system);
